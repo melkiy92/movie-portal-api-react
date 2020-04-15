a6ae42d655dd8f1ff696c03406d35bbb3d3d7768
melkiy92 <melkiy9222@gmail.com>
2020-04-15 22:53:42 +0300

    upload project

---
+++ src/components/MovieTabs.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+class MovieTabs extends React.Component {
+    componentWillReceiveProps(nextProps, nextState) {
+        console.log("MovieTabs willReceiveProps");
+        console.log("nextProps sort_by", nextProps.sort_by);
+        console.log("prevProps sort_by", this.props.sort_by);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.sort_by !== this.props.sort_by) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    render() {
+        const { sort_by, updateSortBy } = this.props;
+
+        const handleClick = value => {
+            return event => {
+                updateSortBy(value);
+            };
+        };
+
+        const getClassLink = value => {
+            return `nav-link ${sort_by === value ? "active" : ""}`;
+        };
+
+        console.log("MovieTabs render");
+
+        return (
+            <ul className="tabs nav nav-pills">
+                <li className="nav-item">
+                    <div
+                        className={getClassLink("popularity.desc")}
+                        onClick={handleClick("popularity.desc")}
+                    >
+                        Popularity desc
+                </div>
+                </li>
+                <li className="nav-item">
+                    <div className={getClassLink("revenue.desc")}
+                        onClick={handleClick("revenue.desc")}
+                    >
+                        Revenue desc
+                </div>
+                </li>
+                <li className="nav-item">
+                    <div
+                        className={getClassLink("vote_average.desc")}
+                        onClick={handleClick("vote_average.desc")}
+                    >
+                        Vote average desc
+                </div>
+                </li>
+            </ul>
+        );
+
+    };
+};
+
+export default MovieTabs;
